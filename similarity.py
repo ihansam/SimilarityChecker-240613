bd8c94f7de6a0889d958c85fb9f91e88974849e7
kidan.jin <ihansam0322@gmail.com>
2024-06-13 15:48:58 +0900

    [feature] 길이 두배 이상

--- similarity.py
+++ similarity.py
@@ -1,4 +1,9 @@
 class SimilarityChecker:
     def examine_length_score(self, string1, string2):
-        if len(string1) == len(string2):
+        longer_length, shorter_length = max(len(string1), len(string2)), min(len(string1), len(string2))
+
+        if longer_length >= 2 * shorter_length:
+            return 0
+
+        if longer_length == shorter_length:
             return 60
--- test_similarity.py
+++ test_similarity.py
@@ -9,3 +9,6 @@ class TestSimilarityChecker(TestCase):
 
     def test_same_length(self):
         self.assertEqual(60, self.checker.examine_length_score("ASD", "DSA"))
+
+    def test_more_than_2x_length(self):
+        self.assertEqual(0, self.checker.examine_length_score("A", "BB"))
