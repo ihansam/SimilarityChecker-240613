d68001a9edd719191893b78da039d6d498fb7210
kidan.jin <ihansam0322@gmail.com>
2024-06-13 16:46:17 +0900

    apply review on #1

--- similarity.py
+++ similarity.py
@@ -1,11 +1,12 @@
 class SimilarityChecker:
+    LENGTH_MAX_SCORE = 60
+
     def examine_length_score(self, string1: str, string2: str) -> float:
+
         longer_length = max(len(string1), len(string2))
         shorter_length = min(len(string1), len(string2))
 
         if longer_length >= 2 * shorter_length:
             return 0
 
-        length_gap = longer_length - shorter_length
-
-        return (1 - length_gap / shorter_length) * 60
+        return (1 - (longer_length - shorter_length) / shorter_length) * self.LENGTH_MAX_SCORE
