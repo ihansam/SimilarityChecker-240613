8e8fc0be153f78fd79a2c7decae7b7f204f203ec
kidan.jin <ihansam0322@gmail.com>
2024-06-13 15:59:51 +0900

    [refactor] 필요 없는 로직 제거

--- similarity.py
+++ similarity.py
@@ -1,15 +1,11 @@
 class SimilarityChecker:
-    def examine_length_score(self, string1, string2):
+    def examine_length_score(self, string1: str, string2: str) -> float:
         longer_length = max(len(string1), len(string2))
         shorter_length = min(len(string1), len(string2))
 
         if longer_length >= 2 * shorter_length:
             return 0
 
-        if longer_length == shorter_length:
-            return 60
+        length_gap = longer_length - shorter_length
 
-        gap = longer_length - shorter_length
-
-        return (1 - gap / shorter_length) * 60
-
+        return (1 - length_gap / shorter_length) * 60
