54b54aeed19b5333a5339daed3e33e9d09f9bfbc
kidan.jin <ihansam0322@gmail.com>
2024-06-13 15:43:28 +0900

    add 틀

---
+++ test_similarity.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from similarity import SimilarityChecker
+
+
+class TestSimilarityChecker(TestCase):
+    def setUp(self):
+        self.checker = SimilarityChecker()
+
+    def test_string_length(self):
+        pass
